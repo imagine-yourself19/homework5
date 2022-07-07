a2b6d3336bc14fae6d084defd7abe129427ecab5
Lena <imagine.youeself19@gmail.com>
2022-07-07 12:36:39 +0300

    решена задача36

---
+++ задача36/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] array = new int[10];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1, 11);
+}
+Console.WriteLine("Выбран  массив" + "[" + string.Join(", ", array) + "]");
+int nechetSum = 0;
+for (int i = 1; i < array.Length; i += 2)
+    {
+        nechetSum = nechetSum + array[i];
+    }
+Console.WriteLine($"Сумма чисел на нечетных позициях равна {nechetSum}");
