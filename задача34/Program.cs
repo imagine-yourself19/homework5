3f3303ec2a2cb6830d360962870f5d21e173709b
Lena <imagine.youeself19@gmail.com>
2022-07-07 12:04:20 +0300

    решена задача34

---
+++ задача34/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] array = new int[10];
+int sum = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100, 1000);
+    if (array[i] % 2 == 0)
+    {
+        sum++;
+    }
+}
+Console.WriteLine("Задан массив " + "[" + string.Join(", ", array) + "]");
+Console.WriteLine($"количество четных чисел в массиве равно {sum}");
