e3bc3f7448d7f8e9b62572876d9cb1e71259b598
Lena <imagine.youeself19@gmail.com>
2022-07-07 12:54:34 +0300

    решена задача38

---
+++ задача38/Program.cs
@@ -0,0 +1,15 @@
+﻿decimal[] array = new decimal[5];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1, 10);
+}
+Console.WriteLine("Выбран массив" + "[" + string.Join(", ", array) + "]");
+decimal max = array[0];
+decimal min = array[0];
+for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] < min) min = array[i];
+        if (array[i] > max) max = array[i];
+    }
+            
+Console.WriteLine($"Разность между максимальным {max} и минимальным {min} равна {max - min}");
